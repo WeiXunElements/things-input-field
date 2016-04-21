6ace2b074130d2898cf1a64f4d7af8a8a12b954a
ThingsElements <things@fly-kites.net>
2016-04-21 17:33:37 +0900

    first commit

---
+++ ThingsElementsPorjectInit.cmd
@@ -0,0 +1,8 @@
+git clone https://github.com/ThingsElements/things-elements.git
+git clone https://github.com/ThingsElements/things-scene-viewer.git
+git clone https://github.com/ThingsElements/things-global-behavior.git
+git clone https://github.com/ThingsElements/things-angle-input.git
+git clone https://github.com/ThingsElements/things-id.git
+git clone https://github.com/ThingsElements/things-designer.git
+git clone https://github.com/ThingsElements/things-starter-kit.git
+
